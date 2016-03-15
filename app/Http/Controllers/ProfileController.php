cf470a9d8d8ff4e38f5307f6a37647cd283a7574
degee <degee147@gmail.com>
2016-03-16 00:59:35 +0100

    commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 
 class ProfileController extends BaseController{
 
@@ -13,6 +15,22 @@ class ProfileController extends BaseController{
 
 
     public function showWelcome(){
+
+
+        $users = DB::select('select * from arts');
+
+/*
+         Schema::create('art', function($newtable)
+         {
+             $newtable->increments('id');
+             $newtable->string('artist');
+             $newtable->string('title',500);
+             $newtable->text('description');
+             $newtable->date('created');
+             $newtable->date('exhibition_date');
+             $newtable->timestamps();
+         });
+*/
         return view('profile');
     }
 }
